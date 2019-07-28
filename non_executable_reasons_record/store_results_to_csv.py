170987962fbd866fb7a29473db9e548b09258b57
KuoTzu-yang <filmsociety1718hkust@gmail.com>
2019-07-28 15:29:48 +1000

    Store results to a csv file & Push corresponding python3 script

---
+++ non_executable_reasons_record/store_results_to_csv.py
@@ -0,0 +1,17 @@
+import csv
+
+# Prepare a csv for writing results 
+with open('results.csv', 'w') as writeFile:
+    writer = csv.writer(writeFile)
+
+    file_str = 'a.txt'
+
+    f = open(file_str, "r")
+    for name in f:
+        target_result_file_name = name.strip() + '.txt'
+        tf = open(target_result_file_name, 'r')
+        content = tf.read()
+        row = [name.strip(), content]
+        writer.writerow(row)
+
+writeFile.close()
