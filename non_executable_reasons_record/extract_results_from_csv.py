d730d4b66df5995ce3f1807407f2cf13808a490c
KuoTzu-yang <filmsociety1718hkust@gmail.com>
2019-07-28 15:34:30 +0800

    Complete results.csv can be correctly extracted by other users

--- non_executable_reasons_record/extract_results_from_csv.py
+++ non_executable_reasons_record/extract_results_from_csv.py
@@ -1,25 +1,9 @@
-import csv
-'''
-# Prepare a csv for writing results 
-with open('results.csv', 'w') as writeFile:
-    writer = csv.writer(writeFile)
-
-    file_str = 'a.txt'
-
-    f = open(file_str, "r")
-    for name in f:
-        target_result_file_name = name.strip() + '.txt'
-        tf = open(target_result_file_name, 'r')
-        content = tf.read()
-        row = [name.strip(), content]
-        writer.writerow(row)
-
-writeFile.close()
-'''
-
-import csv
+import csv, sys
+csv.field_size_limit(sys.maxsize)
 
 with open("results.csv", "r") as f:
-    reader = csv.reader(f, delimiter="\t")
-    for i, line in enumerate(reader):
-        print(i, line)
+    reader = csv.reader(f, delimiter=',')
+    for line in reader:
+        idx, content = line[0], line[1]
+        print(idx)
+        print(content)
