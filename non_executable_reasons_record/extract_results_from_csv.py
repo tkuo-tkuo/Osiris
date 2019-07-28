f43446760acaca0875483e87cbe625ed441fb15e
KuoTzu-yang <filmsociety1718hkust@gmail.com>
2019-07-28 16:44:36 +0800

    Include paths to results.csv additionally

--- non_executable_reasons_record/extract_results_from_csv.py
+++ non_executable_reasons_record/extract_results_from_csv.py
@@ -4,6 +4,7 @@ csv.field_size_limit(sys.maxsize)
 with open("results.csv", "r") as f:
     reader = csv.reader(f, delimiter=',')
     for line in reader:
-        idx, content = line[0], line[1]
-        print(idx)
-        print(content)
+        idx, path, content = line[0], line[1], line[2]
+        print('ID:', idx)
+        print('Path:', path)
+        print('Content:', content)
