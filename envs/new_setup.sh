9f2daa3d2b78fc53fe4363e41faff67f17369084
KuoTzu-yang <filmsociety1718hkust@gmail.com>
2019-07-31 00:45:02 +1000

    Fix bug: python=3.4 not found

--- envs/new_setup.sh
+++ envs/new_setup.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+conda install conda=4.6.11
 conda create -n Osiris_py27 anaconda python=2.7
 conda create -n Osiris_py34 anaconda python=3.4
 conda create -n Osiris_py35 anaconda python=3.5
