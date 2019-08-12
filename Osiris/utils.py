8729b680d8400d6ecd720f9b42d6800778e7523f
KuoTzu-yang <filmsociety1718hkust@gmail.com>
2019-08-12 12:36:15 +1000

    debug: remove unused class

--- Osiris/utils.py
+++ Osiris/utils.py
@@ -4,7 +4,7 @@ import nbformat
 import collections
 import numpy as np
 
-from .dependency_graph import DependencyGraph, CDG
+from .dependency_graph import CDG
 from .dependency_graph import get_code_list, detect, get_antidote, get_path_by_extension, find_local_modules, get_oec
 
 '''
